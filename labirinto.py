9c16484c02a8ee0ad1c7f5db38f2c3151da3d3ac
Rehd96 <96469834+Rehd96@users.noreply.github.com>
2022-02-15 21:31:15 +0100

    terza_modifica
    Implementato spostamento del giocatore.
    Intricato leggermente il labirinto

--- labirinto.py
+++ labirinto.py
@@ -3,13 +3,14 @@ import random
 class Labirinto:
     nomi = ['Parcheggio','Atrio','Area relax','Segreteria','Aula Azzurra','Aula Gialla','Presidenza']
     esplorate = []
+    
     x= -1
     y = -1
     labirinto = [
         ['.','.','.','x','.'],
         ['.','.','x','x','.'],
-        ['.','x','x','x','.'],
-        ['.','.','x','.','.'],
+        ['.','x','x','.','.'],
+        ['.','x','x','.','.'],
         ['.','.','.','.','.']
         ]
     mappa = []
@@ -23,14 +24,16 @@ class Labirinto:
                 c+=num
             else:
                 for colonna in range(len(self.labirinto[riga])):
-                    if self.labirinto[riga][colonna] == 'x':
+                    if p!= -1:
+                        if self.labirinto[riga][colonna] == 'x':
+                            
+                            if c==p:
+                                self.x = riga
+                                self.y = colonna
+                                self.esplorate.append([riga,colonna])
+                                p = -1
+                            c+=1
                         
-                        if c==p:
-                            self.x = riga
-                            self.y = colonna
-                            self.esplorate.append([riga,colonna])
-                            p = -1
-                        c+=1
         nome = input("Come ti chiami? \n")
         self.nome = nome
         for x in range(len(self.labirinto)):
@@ -39,13 +42,36 @@ class Labirinto:
                 riga.append('.')
             self.mappa.append(riga)
         print("Benvenuto, ",self.nome," ,ti trovi qui \n \n")
-        self.mappa[self.x][self.y] = 'x'
+        self.mappa[self.x][self.y] = '!'
         for riga in self.mappa:
             print(riga)
                         
-                        
+    def move(self,place):
+        place = place.upper()
+        if place not in ['S','N','O','E']:
+            cond = False
+            while cond==False:
+                print("\n Non puoi andare lì, sciocchino. Scegli una direzione valida \n")
+                place = input()
+                place = place.upper()
+                if place in ['S','N','O','E']:
+                    cond= True
+        self.mappa[self.x][self.y] = 'x'
+        if place=='N':
+            
+            self.x-=1
+        elif place == 'S':
+            self.x +=1
+        elif place == 'E':
+            self.y+=1
+        else:
+            self.y-=1
+        self.esplorate.append([self.x,self.y])
+        self.mappa[self.x][self.y] = '!'
+        
+            
     def show(self):
-        for riga in self.labirinto:
+        for riga in self.mappa:
             print(riga)
             
     
--- play.py
+++ play.py
@@ -1,3 +1,4 @@
 from labirinto import Labirinto
 
 d = Labirinto()
+d.move('p')
